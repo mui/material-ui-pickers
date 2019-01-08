020fb5236d9c0e996b320b0fe5a99b4b9eef11c6
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-01-08 16:42:17 +0200

    Add unit test for MuiIcon.test.tsx

--- lib/src/__tests__/_shared/MuiIcon.test.tsx
+++ lib/src/__tests__/_shared/MuiIcon.test.tsx
@@ -1,1 +1,21 @@
-describe('MuiIcon', () => {});
+import { mount } from 'enzyme';
+import React from 'react';
+
+describe('MuiIcon', () => {
+  it('Should render @material-ui/icons by default', () => {
+    const MuiIcon = require('../../_shared/MuiIcon').default;
+    const component = mount(<MuiIcon icon="event" />);
+
+    expect(component.find('svg').length).toBe(1);
+  });
+
+  it('Should render string if there is no @material-ui/icons', () => {
+    jest.resetModules();
+    jest.mock('@material-ui/icons/Event', () => null);
+
+    const MuiIcon = require('../../_shared/MuiIcon').default;
+    const component = mount(<MuiIcon icon="event" />);
+
+    expect(component.find('svg').length).toBe(0);
+  });
+});
