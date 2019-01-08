765b4cae543dbca16ccad13bbc4805b833f35272
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-01-08 16:48:53 +0200

    Move import of MuiIcon to the top for not mocked test

--- lib/src/__tests__/_shared/MuiIcon.test.tsx
+++ lib/src/__tests__/_shared/MuiIcon.test.tsx
@@ -1,9 +1,9 @@
 import { mount } from 'enzyme';
 import React from 'react';
+import MuiIcon from '../../_shared/MuiIcon';
 
 describe('MuiIcon', () => {
   it('Should render @material-ui/icons by default', () => {
-    const MuiIcon = require('../../_shared/MuiIcon').default;
     const component = mount(<MuiIcon icon="event" />);
 
     expect(component.find('svg').length).toBe(1);
@@ -13,8 +13,8 @@ describe('MuiIcon', () => {
     jest.resetModules();
     jest.mock('@material-ui/icons/Event', () => null);
 
-    const MuiIcon = require('../../_shared/MuiIcon').default;
-    const component = mount(<MuiIcon icon="event" />);
+    const MuiIconWithoutPackage = require('../../_shared/MuiIcon').default;
+    const component = mount(<MuiIconWithoutPackage icon="event" />);
 
     expect(component.find('svg').length).toBe(0);
   });
