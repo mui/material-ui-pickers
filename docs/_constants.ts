3f8fcdace0d8cb4c2b7e8f4e6edfb1b4be2cbee5
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-05-02 16:56:39 +0300

    Fix HOST_NAME constant

--- docs/_constants.ts
+++ docs/_constants.ts
@@ -1,6 +1,6 @@
 export const CODESANDBOX_EXAMPLE_ID = 'o7oojxx1pq';
 
-export const HOST_URL = 'https://@material-ui/pickers.dev';
+export const HOST_URL = 'https://material-ui-pickers.dev';
 
 export const LOGO_URL = HOST_URL + '/static/meta-image.png';
 
