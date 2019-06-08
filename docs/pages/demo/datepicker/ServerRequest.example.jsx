19a863543f4427ecd78864875178387b8c5f1cc6
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-06-08 15:27:36 +0300

    Change text and remove unnecessary details from example

--- docs/pages/demo/datepicker/ServerRequest.example.jsx
+++ docs/pages/demo/datepicker/ServerRequest.example.jsx
@@ -1,14 +1,13 @@
 import React, { Fragment, useState } from 'react';
 import { Badge } from '@material-ui/core';
+import { DatePicker } from '@material-ui/pickers';
 import { makeJSDateObject } from '../../../utils/helpers';
-import { DatePicker, Day, useUtils } from '@material-ui/pickers';
 
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
 function ServerRequest() {
-  const utils = useUtils();
   const [selectedDays, setSelectedDays] = useState([1, 2, 15]);
   const [selectedDate, handleDateChange] = useState(new Date());
 
@@ -24,21 +23,16 @@ function ServerRequest() {
   return (
     <Fragment>
       <DatePicker
-        label="Server"
+        label="With server data"
         value={selectedDate}
         onChange={handleDateChange}
         onMonthChange={handleMonthChange}
-        renderDay={(day, selected, isInCurrentMonth) => {
+        renderDay={(day, selectedDate, isInCurrentMonth, dayComponent) => {
           const date = makeJSDateObject(day); // skip this step, it is required to support date libs
           const isSelected = isInCurrentMonth && selectedDays.includes(date.getDate());
 
-          return (
-            <Badge badgeContent={isSelected ? '🌚' : undefined}>
-              <Day selected={utils.isSameDay(day, selected)} hidden={!isInCurrentMonth}>
-                {utils.getDayText(day)}
-              </Day>
-            </Badge>
-          );
+          // You can also use our internal <Day /> component
+          return <Badge badgeContent={isSelected ? '🌚' : undefined}>{dayComponent}</Badge>;
         }}
       />
     </Fragment>
