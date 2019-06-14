8124705e2a3e852a55a85294e55152d3af7cb0a2
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-06-14 14:47:31 +0300

    Make proper implementation of arrayIncludes 🙆‍♂️

--- lib/src/_helpers/utils.ts
+++ lib/src/_helpers/utils.ts
@@ -1,4 +1,4 @@
 /** Use it instead of .includes method for IE support */
 export function arrayIncludes<T>(array: T[], item: T) {
-  return array.indexOf(item) > 0;
+  return array.indexOf(item) !== -1;
 }
