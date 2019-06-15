3d4039fc99377f94a800232be40a6cc6a6d5be6d
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-06-15 12:51:04 +0300

    Add more visual regression scenarios

--- e2e/integration/VisualRegression.spec.ts
+++ e2e/integration/VisualRegression.spec.ts
@@ -13,20 +13,49 @@ describe('Visual Regression', () => {
       url: '/demo/datepicker',
       name: 'DatePicker demo',
       withDarkTheme: true,
+      scenarios: [
+        {
+          name: 'Opened datepicker',
+          execute: () => {
+            cy.get('[data-test-id=datepicker-example]')
+              .find('input')
+              .first()
+              .click();
+          },
+        },
+      ],
+    },
+    {
+      url: '/demo/timepicker',
+      name: 'TimePicker demo',
+    },
+    {
+      url: '/demo/datetime-picker',
+      name: 'DateTimePicker demo',
+    },
+    {
+      url: '/guides/css-overrides',
+      name: 'Css overrides',
+      scenarios: [
+        {
+          name: 'Custom material-ui theme',
+          execute: () => {
+            cy.get('[data-test-id=css-override]')
+              .find('input')
+              .click();
+          },
+        },
+      ],
     },
   ];
-
-  before(() => {
-    const now = new Date(2018, 1, 1);
-    cy.clock(now.getMilliseconds());
-  });
 
   pages.forEach(page => {
     context(page.name, () => {
       before(() => {
-        cy.visit(page.url);
         const now = new Date(2018, 1, 1);
         cy.clock(now.getMilliseconds());
+
+        cy.visit(page.url);
       });
 
       it(`Displays ${page.name} page`, () => {
@@ -35,8 +64,26 @@ describe('Visual Regression', () => {
 
       if (page.withDarkTheme) {
         it(`Displays ${page.name} page in dark theme`, () => {
-          cy.get('[data-testid=toggle-theme-btn]').click();
+          cy.toggleTheme();
           cy.percySnapshot(`Dark ${page.name}`);
+          cy.toggleTheme();
+        });
+      }
+
+      if (page.scenarios) {
+        page.scenarios.forEach(hook => {
+          it(`${page.name} scenario: ${hook.name}`, () => {
+            hook.execute();
+            cy.percySnapshot(`${page.name}: ${hook.name}`);
+          });
+
+          if (page.withDarkTheme) {
+            it(`${page.name} scenario: ${hook.name} in dark theme`, () => {
+              cy.toggleTheme({ force: true });
+              cy.percySnapshot(`Dark ${page.name}: ${hook.name}`);
+              cy.toggleTheme({ force: true });
+            });
+          }
         });
       }
     });
