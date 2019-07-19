372deb466e15d027f44ac69339ced04ab93d8b4e
igornfaustino <igornfaustino@gmail.com>
2019-07-19 20:03:23 -0300

    Wrap example with redux

--- docs/pages/guides/ReduxForm.example.jsx
+++ docs/pages/guides/ReduxForm.example.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import store from './store';
 import Code from '../../_shared/Code';
 import { connect } from 'react-redux';
 import { Grid } from '@material-ui/core';
+import { Provider as ReduxProvider } from 'react-redux';
 import { KeyboardDatePicker } from '@material-ui/pickers';
 import { reduxForm, Field, formValueSelector } from 'redux-form';
 
@@ -26,39 +28,49 @@ const DateField = props => {
   );
 };
 
-class ReduxFormExample extends React.PureComponent {
-  render() {
-    return (
-      <form>
-        <Grid container>
-          <Grid item container justify="center" xs={12}>
-            <Field name="date" component={DateField} />
-          </Grid>
-          <Grid item xs={12} sm={12} style={{ margin: '24px' }}>
-            <Code
-              children={JSON.stringify(
-                {
-                  values: {
-                    date: this.props.date,
-                  },
-                },
-                null,
-                2
-              )}
-            />
-          </Grid>
+const ReduxFormExample = props => {
+  const formValues = {
+    isFormValid: props.valid,
+    values: {
+      date: props.date,
+    },
+  };
+
+  const submit = values => {
+    alert(JSON.stringify(values));
+  };
+
+  return (
+    // only calls the submit if form is valid
+    <form onSubmit={props.handleSubmit(submit)}>
+      <Grid container>
+        <Grid item container justify="center" xs={12}>
+          <Field name="date" component={DateField} />
         </Grid>
-      </form>
-    );
-  }
-}
+        <Grid item xs={12} sm={12} style={{ margin: '24px' }}>
+          <Code children={JSON.stringify(formValues, null, 2)} />
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
 
 const selector = formValueSelector('example');
+
 const mapStateToProps = state => ({
   date: selector(state, 'date'),
   initialValues: {
     date: new Date().toISOString(),
   },
 });
+
 const createReduxForm = reduxForm({ form: 'example' });
-export default connect(mapStateToProps)(createReduxForm(ReduxFormExample));
+const Form = connect(mapStateToProps)(createReduxForm(ReduxFormExample));
+
+const app = () => (
+  <ReduxProvider store={store}>
+    <Form />
+  </ReduxProvider>
+);
+
+export default app;
