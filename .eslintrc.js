1224394f52ca245f419e74c53aa31884609cd754
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2019-12-17 18:28:23 +0200

    Turn off empty interface rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
       { vars: 'all', args: 'after-used', argsIgnorePattern: '^_', ignoreRestSiblings: true },
