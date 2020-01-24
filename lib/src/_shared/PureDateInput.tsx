ebf17597513671328096f6f8028337057e0a64e1
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-01-24 22:38:00 +0100

    Fix typescript typings using TextFieldProps (#1469)

--- lib/src/_shared/PureDateInput.tsx
+++ lib/src/_shared/PureDateInput.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import TextField, { TextFieldProps } from '@material-ui/core/TextField';
+import TextField, { BaseTextFieldProps, TextFieldProps } from '@material-ui/core/TextField';
 import { ExtendMui } from '../typings/extendMui';
 
 export type NotOverridableProps =
@@ -12,13 +12,15 @@ export type NotOverridableProps =
   | 'forwardedRef';
 
 export interface PureDateInputProps
-  extends ExtendMui<TextFieldProps, 'variant' | 'onError' | 'onChange' | 'value'> {
+  extends ExtendMui<BaseTextFieldProps, 'variant' | 'onError' | 'value'> {
   /** Pass material-ui text field variant down, bypass internal variant prop */
   inputVariant?: TextFieldProps['variant'];
   /** Override input component */
   TextFieldComponent?: React.ComponentType<TextFieldProps>;
   InputProps?: TextFieldProps['InputProps'];
   inputProps?: TextFieldProps['inputProps'];
+  onBlur?: TextFieldProps['onBlur'];
+  onFocus?: TextFieldProps['onFocus'];
   inputValue: string;
   validationError?: React.ReactNode;
   openPicker: () => void;
