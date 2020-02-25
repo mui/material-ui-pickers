7ff010db41fbca07a08944562f8665372b58acb8
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-02-25 21:05:12 +0200

    Implement global format override

--- docs/pages/guides/Formats.example.jsx
+++ docs/pages/guides/Formats.example.jsx
@@ -1,25 +1,23 @@
-import format from 'date-fns/format';
 import React, { useState } from 'react';
 import frLocale from 'date-fns/locale/fr';
 import DateFnsAdapter from '@material-ui/pickers/adapter/date-fns';
 import { DatePicker } from '@material-ui/pickers';
 import { LocalizationProvider } from '@material-ui/pickers';
 
-class LocalizedUtils extends DateFnsAdapter {
-  getDatePickerHeaderText(date) {
-    return format(date, 'd MMM yyyy', { locale: this.locale });
-  }
-}
+/** @type Partial<import('@date-io/core/IUtils').DateIOFormats> */
+const formats = {
+  normalDate: 'd MMM yyy',
+  keyboardDate: 'd MMM yyy',
+};
 
 function DateFnsLocalizationExample() {
   const [selectedDate, handleDateChange] = useState(new Date());
 
   return (
-    <LocalizationProvider adapter={LocalizedUtils} locale={frLocale}>
+    <LocalizationProvider adapter={DateFnsAdapter} locale={frLocale} libFormats={formats}>
       <DatePicker
         clearable
         helperText="Localization done right"
-        inputFormat="d MMM yyyy"
         value={selectedDate}
         onChange={handleDateChange}
         clearLabel="vider"
--- lib/src/LocalizationProvider.tsx
+++ lib/src/LocalizationProvider.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import { DateIOFormats } from '@date-io/core/IUtils';
 import { MuiPickersUtils } from './_shared/hooks/useUtils';
 
 export const MuiPickersAdapterContext = React.createContext<MuiPickersUtils | null>(null);
@@ -9,19 +10,20 @@ export interface LocalizationProviderProps {
   children: React.ReactNode;
   locale?: any;
   libInstance?: any;
+  libFormats?: Partial<DateIOFormats>;
 }
 
 export const LocalizationProvider: React.FC<LocalizationProviderProps> = ({
   adapter: Utils,
   children,
   locale,
+  libFormats,
   libInstance,
 }) => {
-  const utils = React.useMemo(() => new Utils({ locale, instance: libInstance }), [
-    Utils,
-    libInstance,
-    locale,
-  ]);
+  const utils = React.useMemo(
+    () => new Utils({ locale, formats: libFormats, instance: libInstance }),
+    [Utils, locale, libFormats, libInstance]
+  );
 
   return <MuiPickersAdapterContext.Provider value={utils} children={children} />;
 };
