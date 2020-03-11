eb95e4dbd0dea73f2d80e1c9db5f96d49fb24889
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-03-11 18:43:09 +0200

    Remove DateRangePicker code

--- lib/src/DateRangePicker/RangeTypes.ts
+++
@@ -1,5 +0,0 @@
-import { ParsableDate } from "../constants/prop-types";
-import { MaterialUiPickersDate } from "../typings/date";
-
-export type RangeInput = [ParsableDate, ParsableDate];
-export type DateRange = [MaterialUiPickersDate, MaterialUiPickersDate];
