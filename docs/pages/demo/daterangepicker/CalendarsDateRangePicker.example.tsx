a9fd7ac537ed9771fb4100ad8a04841f99a87440
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-04-07 16:15:27 +0300

    Update docs/pages/demo/daterangepicker/CalendarsDateRangePicker.example.tsx
    Co-Authored-By: Olivier Tassinari <olivier.tassinari@gmail.com>

--- docs/pages/demo/daterangepicker/CalendarsDateRangePicker.example.tsx
+++ docs/pages/demo/daterangepicker/CalendarsDateRangePicker.example.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import { DateRangePicker, DateRange } from '@material-ui/pickers';
 
 function CalendarsDateRangePicker() {
