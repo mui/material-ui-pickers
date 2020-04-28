070deaee10d7e598cf8527e9fd73e9413c0e18d3
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-04-28 20:36:14 +0300

    Fix bugs found by tests

--- lib/src/_shared/hooks/useMaskedInput.tsx
+++ lib/src/_shared/hooks/useMaskedInput.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { useRifm } from 'rifm';
 import { useUtils } from './useUtils';
-import { DateInputProps } from '../PureDateInput';
 import { createDelegatedEventHandler } from '../../_helpers/utils';
+import { DateInputProps, MuiTextFieldProps } from '../PureDateInput';
 import {
   maskedDateFormatter,
   getDisplayDate,
@@ -38,7 +38,7 @@ export function useMaskedInput({
   readOnly,
   TextFieldProps,
   label,
-}: MaskedInputProps) {
+}: MaskedInputProps): MuiTextFieldProps {
   const utils = useUtils();
   const isFocusedRef = React.useRef(false);
 
@@ -52,7 +52,7 @@ export function useMaskedInput({
   );
 
   const formatHelperText = utils.getFormatHelperText(inputFormat);
-  const [innerInputValue, setInnerInputValue] = React.useState<string | null>(getInputValue());
+  const [innerInputValue, setInnerInputValue] = React.useState<string>(getInputValue());
 
   const shouldUseMaskedInput = React.useMemo(() => {
     // formatting of dates is a quite slow thing, so do not make useless .format calls
@@ -78,7 +78,7 @@ export function useMaskedInput({
   }, [utils, getInputValue, rawValue]);
 
   const handleChange = (text: string) => {
-    const finalString = text === '' || text === mask ? null : text;
+    const finalString = text === '' || text === mask ? '' : text;
     setInnerInputValue(finalString);
 
     const date = finalString === null ? null : utils.parse(finalString, inputFormat);
@@ -90,19 +90,27 @@ export function useMaskedInput({
   };
 
   const rifmProps = useRifm({
-    value: innerInputValue || '',
+    value: innerInputValue,
     onChange: handleChange,
     format: rifmFormatter || formatter,
   });
 
+  const inputStateArgs = shouldUseMaskedInput
+    ? rifmProps
+    : {
+        value: innerInputValue,
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => handleChange(e.currentTarget.value),
+      };
+
   return {
-    ...(shouldUseMaskedInput ? rifmProps : {}),
+    ...inputStateArgs,
     label,
     disabled,
     type: shouldUseMaskedInput ? 'tel' : 'text',
     placeholder: formatHelperText,
     error: Boolean(validationError),
     helperText: formatHelperText || validationError,
+    // @ts-ignore ??? fix typings for textfield finally
     'data-mui-test': 'keyboard-date-input',
     inputProps: { readOnly },
     ...TextFieldProps,
