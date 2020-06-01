d9cef63c0b57216e776d921d16e2cc45abce6338
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-06-01 12:20:50 +0300

    Revert BasicDatePicker changes

--- docs/pages/demo/datepicker/BasicDatePicker.example.jsx
+++ docs/pages/demo/datepicker/BasicDatePicker.example.jsx
@@ -3,16 +3,12 @@ import { TextField } from '@material-ui/core';
 import { DatePicker } from '@material-ui/pickers';
 
 function BasicDatePicker() {
-  const [selectedDate, handleDateChange] = useState(null);
+  const [selectedDate, handleDateChange] = useState(new Date());
 
   return (
     <DatePicker
-      open
       label="Basic example"
       value={selectedDate}
-      views={['year']}
-      minDate={new Date("2000-01-01")}
-      maxDate={new Date("2010-01-01")}
       onChange={date => handleDateChange(date)}
       renderInput={props => <TextField {...props} />}
     />
