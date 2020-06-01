3a3a1c2798413fe7be02c2a7e16541729173269c
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-06-01 12:07:20 +0300

    Fix moment and luxon issue when mounting year selection view

--- docs/pages/demo/datepicker/BasicDatePicker.example.jsx
+++ docs/pages/demo/datepicker/BasicDatePicker.example.jsx
@@ -3,12 +3,16 @@ import { TextField } from '@material-ui/core';
 import { DatePicker } from '@material-ui/pickers';
 
 function BasicDatePicker() {
-  const [selectedDate, handleDateChange] = useState(new Date());
+  const [selectedDate, handleDateChange] = useState(null);
 
   return (
     <DatePicker
+      open
       label="Basic example"
       value={selectedDate}
+      views={['year']}
+      minDate={new Date("2000-01-01")}
+      maxDate={new Date("2010-01-01")}
       onChange={date => handleDateChange(date)}
       renderInput={props => <TextField {...props} />}
     />
