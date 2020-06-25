7de39badfcbf411a68341dfaec901f512ad0b4d3
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-06-25 13:18:26 +0300

    Rename mui component name of PickerPopper

--- lib/src/_shared/PickerPopper.tsx
+++ lib/src/_shared/PickerPopper.tsx
@@ -49,7 +49,7 @@ export const useStyles = makeStyles(
       transformOrigin: 'bottom center',
     },
   }),
-  { name: 'MuiPickersPopper' }
+  { name: 'MuiPickerPopper' }
 );
 
 export const PickerPopper: React.FC<PickerPopperProps> = ({
