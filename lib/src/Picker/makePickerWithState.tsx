7e088d51f8e5c3fec06ab5af71afdbcd3c3aed84
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-07-10 14:06:35 +0300

    Update lib/src/Picker/makePickerWithState.tsx
    Co-authored-by: Olivier Tassinari <olivier.tassinari@gmail.com>

--- lib/src/Picker/makePickerWithState.tsx
+++ lib/src/Picker/makePickerWithState.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Picker, { ExportedPickerProps } from './Picker';
-
 import { ParsableDate } from '../constants/prop-types';
 import { MuiPickersAdapter } from '../_shared/hooks/useUtils';
 import { parsePickerInputValue } from '../_helpers/date-utils';
