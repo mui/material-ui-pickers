4e5401fbe75beb84dd7243fa151b90d13dad3d2f
Jan Šilhan <silhanu@gmail.com>
2020-08-24 21:12:36 +0200

    fix: Order of import.

--- lib/src/__tests__/Theme.test.tsx
+++ lib/src/__tests__/Theme.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { mount } from './test-utils';
 import { DatePicker } from '../DatePicker';
 import { DateTimePicker } from '../DateTimePicker/DateTimePicker';
